849fc141b1938ea45aa33c8c9fb2ecf08a1072a8
rtborg <b.kereziev@gmail.com>
2021-01-08 15:24:55 +0200

    Code cleanup

--- PFMB_Project_Software/Src/main.c
+++ PFMB_Project_Software/Src/main.c
@@ -1,22 +1,6 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-
+/*
+ * PFMB7201 0-5V to RS485 Converter board
+ * */
 
 /*
  * JTAG CONNECTION. See https://www.segger.com/downloads/application-notes/AN00021
@@ -33,21 +17,13 @@
  * 10 - Njtrst						3 - nTRST
  * */
 
-/* USER CODE END Header */
-
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
-/* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
 //Modbus command structure definition and buffer
 typedef struct ModbusCommand {
 	uint8_t		address;
@@ -58,29 +34,21 @@ typedef struct ModbusCommand {
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
 #define UART_RX_BUFFER_LEN	8
 #define UART_TX_BUFFER_LEN	64
 #define MODBUS_COMMAND_BUFFER_LEN	8
-/* USER CODE END PD */
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-/* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc2;
-
 CRC_HandleTypeDef hcrc;
-
 OPAMP_HandleTypeDef hopamp2;
-
 UART_HandleTypeDef huart1;
-
-/* USER CODE BEGIN PV */
 uint32_t modbus_address = 0;
 float adc_value;
+
 /* UART receive buffer */
+// NOTE: Buffers are volatile, modified in interrupts
 static volatile uint8_t uart_rx_buffer[UART_RX_BUFFER_LEN];
 static volatile uint8_t uart_rx_buffer_head = 0;
 static volatile uint8_t uart_rx_buffer_tail = 0;
@@ -104,8 +72,6 @@ static volatile uint8_t modbus_command_buffer_count = 0;
 static float sensor_step_per_liter = 0;
 static float sensor_zero_offset = 0;
 
-/* USER CODE END PV */
-
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
@@ -113,7 +79,7 @@ static void MX_ADC2_Init(void);
 static void MX_OPAMP2_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_CRC_Init(void);
-/* USER CODE BEGIN PFP */
+
 /* Modbus functions */
 uint8_t get_modbus_address();
 ModbusCommand get_modbus_command();
@@ -130,47 +96,21 @@ bool uart_has_data();
 void uart_putchar(uint8_t ch);
 void uart_putstring(uint8_t *s, uint8_t size);
 
-/* USER CODE END PFP */
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-  
 
   /* MCU Configuration--------------------------------------------------------*/
-
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
   /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC2_Init();
   MX_OPAMP2_Init();
   MX_USART1_UART_Init();
   MX_CRC_Init();
-  /* USER CODE BEGIN 2 */
   HAL_OPAMP_Start(&hopamp2);
 
   modbus_address = get_modbus_address();
@@ -179,23 +119,9 @@ int main(void)
 	  Error_Handler();
   }
 
-  // HAL_UART_Receive_IT(&huart1, uart_rx_buffer, UART_RX_BUFFER_LEN);
 
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-	  //HAL_Delay(100);
-	  //HAL_GPIO_TogglePin(Keepalive_LED_GPIO_Port, Keepalive_LED_Pin);
-	  //adc_value = get_adc_value();
-
-	 // HAL_UART_Transmit(&huart1, "Hello World!\n\r", 14, 100);
-
 	  // Check if a message is received
 	  if ( modbus_command_buffer_count != 0 ) {
 		  // Get modbus command from buffer
@@ -229,7 +155,6 @@ int main(void)
 	  }
 
   }
-  /* USER CODE END 3 */
 }
 
 /**
@@ -284,16 +209,7 @@ void SystemClock_Config(void)
   */
 static void MX_ADC2_Init(void)
 {
-
-  /* USER CODE BEGIN ADC2_Init 0 */
-
-  /* USER CODE END ADC2_Init 0 */
-
   ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC2_Init 1 */
-
-  /* USER CODE END ADC2_Init 1 */
   /** Common config 
   */
   hadc2.Instance = ADC2;
@@ -343,14 +259,6 @@ static void MX_ADC2_Init(void)
   */
 static void MX_CRC_Init(void)
 {
-
-  /* USER CODE BEGIN CRC_Init 0 */
-
-  /* USER CODE END CRC_Init 0 */
-
-  /* USER CODE BEGIN CRC_Init 1 */
-
-  /* USER CODE END CRC_Init 1 */
   hcrc.Instance = CRC;
   hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_DISABLE;
   hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_DISABLE;
@@ -364,10 +272,6 @@ static void MX_CRC_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN CRC_Init 2 */
-
-  /* USER CODE END CRC_Init 2 */
-
 }
 
 /**
@@ -377,14 +281,6 @@ static void MX_CRC_Init(void)
   */
 static void MX_OPAMP2_Init(void)
 {
-
-  /* USER CODE BEGIN OPAMP2_Init 0 */
-
-  /* USER CODE END OPAMP2_Init 0 */
-
-  /* USER CODE BEGIN OPAMP2_Init 1 */
-
-  /* USER CODE END OPAMP2_Init 1 */
   hopamp2.Instance = OPAMP2;
   hopamp2.Init.Mode = OPAMP_FOLLOWER_MODE;
   hopamp2.Init.NonInvertingInput = OPAMP_NONINVERTINGINPUT_IO2;
@@ -394,9 +290,6 @@ static void MX_OPAMP2_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN OPAMP2_Init 2 */
-
-  /* USER CODE END OPAMP2_Init 2 */
 
 }
 
@@ -407,14 +300,6 @@ static void MX_OPAMP2_Init(void)
   */
 static void MX_USART1_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 9600;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -669,35 +554,12 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
 	}
 }
 
-/* USER CODE END 4 */
-
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-
-  /* USER CODE END Error_Handler_Debug */
+	while(1);
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(char *file, uint32_t line)
-{ 
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
